12966fefac8b5020ae8408b53a5f07bf59a615cf
Marcelo Dapper <marcelo.dapper@gmail.com>
2020-09-05 16:45:33 -0300

    Undo changes

--- src/index.js
+++ src/index.js
@@ -9,8 +9,7 @@ ReactDOM.render(
   document.getElementById('root')
 ); */
 
-/*
-const rootEl = document.getElementById('root');
+/* const rootEl = document.getElementById('root');
 const h1 = document.createElement('h1');
 h1.textContent = 'Introdução ao React';
 h1.className = 'title';
@@ -19,18 +18,10 @@ rootEl.appendChild(h1); */
 
 const rootEl = document.getElementById('root');
 
-/* const h1 = React.createElement('h1', {
+const h1 = React.createElement('h1', {
   className: 'title',
   children: React.createElement('span', null, 'Introdução ao React'),
-}, text); */
-
-// JSX
-const text = 'Introdução ao React';
-const className = 'title';
-const h1 = (
-  <h1 className={className}>
-    <span>{text}</span>
-  </h1>
-);
+});
+console.log(h1);
 
 ReactDOM.render(h1, rootEl);
