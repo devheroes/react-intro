ebe40ae6420413e27ac2cbef43a62991ec72791c
devheroes <61661753+devheroes@users.noreply.github.com>
2020-09-05 17:36:43 -0300

    Video 03 - Como o React funciona internamente (#2)
    No vídeo #03 da série Introdução ao React vemos como o React funciona internamente para renderizar os componentes na tela.
    
    Nele vemos como adicionar um elemento ao DOM usando apenas JavaScript e depois como fazer o mesmo usando a API `createElement` do React.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+/* import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
-);
+); */
+
+/* const rootEl = document.getElementById('root');
+const h1 = document.createElement('h1');
+h1.textContent = 'Introdução ao React';
+h1.className = 'title';
+
+rootEl.appendChild(h1); */
+
+const rootEl = document.getElementById('root');
+
+const h1 = React.createElement('h1', {
+  className: 'title',
+  children: React.createElement('span', null, 'Introdução ao React'),
+});
+console.log(h1);
+
+ReactDOM.render(h1, rootEl);
