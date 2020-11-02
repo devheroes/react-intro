a70ec1bf4693832b953140956ffba2c7d9142317
devheroes <61661753+devheroes@users.noreply.github.com>
2020-11-02 11:13:33 -0300

    Add component Welcome (#4)
    Co-authored-by: Marcelo Dapper <marcelo.dapper@gmail.com>
    
    No [vídeo #05](https://youtu.be/UbdhQrWgc8g) da série Introdução ao React vemos como criar nossos primeiros componentes React e aprendemos sobre o conceito de propriedades, ou props.
    
    Vemos como criar componentes React usando function, arrow function e class.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
 import React from 'react';
 
+function Welcome({ firstName, lastName }) {
+  return (
+    <h2>
+      Bem vindo {firstName} {lastName}
+    </h2>
+  );
+}
+
+/* const Welcome = ({ firstName, lastName }) => (
+  <h2>
+    Bem vindo {firstName} {lastName}!
+  </h2>
+); */
+
+/* class Welcome extends Component {
+  render() {
+    const { firstName, lastName } = this.props;
+    return (
+      <h2>
+        Bem vindo {firstName} {lastName}!
+      </h2>
+    );
+  }
+} */
+
 function App() {
   return (
     <div>
       <h1>Introdução ao React</h1>
+      <Welcome
+        firstName="Marcelo"
+        lastName="Dapper"
+      />
     </div>
   );
 }
