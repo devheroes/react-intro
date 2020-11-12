288df9238b28a1b48ca4f6f55a94884e41be17f7
devheroes <61661753+devheroes@users.noreply.github.com>
2020-11-12 13:53:18 -0300

    Add conditional and fragment (#5)
    Co-authored-by: Marcelo Dapper <marcelo.dapper@gmail.com>

--- src/App.js
+++ src/App.js
@@ -2,37 +2,25 @@ import React from 'react';
 
 function Welcome({ firstName, lastName }) {
   return (
-    <h2>
-      Bem vindo {firstName} {lastName}
-    </h2>
+    <>
+      <h2>
+        Bem vindo {firstName} {lastName}
+      </h2>
+      <p>Você não tem novas notificações.</p>
+    </>
   );
 }
 
-/* const Welcome = ({ firstName, lastName }) => (
-  <h2>
-    Bem vindo {firstName} {lastName}!
-  </h2>
-); */
-
-/* class Welcome extends Component {
-  render() {
-    const { firstName, lastName } = this.props;
-    return (
-      <h2>
-        Bem vindo {firstName} {lastName}!
-      </h2>
-    );
-  }
-} */
-
-function App() {
+function App({ isLoggedIn }) {
   return (
     <div>
       <h1>Introdução ao React</h1>
-      <Welcome
-        firstName="Marcelo"
-        lastName="Dapper"
-      />
+      {isLoggedIn && (
+        <Welcome
+          firstName="Marcelo"
+          lastName="Dapper"
+        />
+      )}
     </div>
   );
 }
